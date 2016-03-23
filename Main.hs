ca1dae04027b10b4b9a0963593e7249f47a33aba
András Kovács <puttamalac@gmail.com>
2016-03-24 00:42:06 +0100

    Removed loop elim because it slows us down
    I don't really know why the slowdown happens.

--- Main.hs
+++ Main.hs
@@ -3,8 +3,6 @@
   PatternSynonyms, ViewPatterns, UnboxedTuples,
   ScopedTypeVariables, NoMonomorphismRestriction #-}
 
--- {-# options_ghc -Wall #-}
-
 import Control.Applicative
 import Control.DeepSeq
 import Control.Monad.State.Strict
@@ -20,19 +18,6 @@ import qualified Data.Attoparsec.ByteString.Char8 as P
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as MV
-
-import Debug.Trace
-import Control.Concurrent
-
-
-hello = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
-
-squares = "++++[>+++++<-]>[<+++++>-]+<+[>[>+>+<<-]++>>[<<+>>-]>>>[-]++>[-]+>>>+[[-]++++++>>>]<<<[[<++++++++<++>>-]+<.<[>----<-]<]<<[>>>>>[>>>[-]+++++++++<[>-<-]+++++++++>[-[<->-]+[<<<]]<[>+<-]>]<<-]<<-]"
-
-hello2 = ">++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+."
-
-cellSize = "++++++++[>++++++++<-]>[<++++>-]+<[>-<[>++++<-]>[<++++++++>-]<[>++++++++<-]+>[>++++++++++[>+++++<-]>+.-.[-]<<[-]<->]<[>>+++++++[>+++++++<-]>.+++++.[-]<<<-]]>[>++++++++[>+++++++<-]>.[-]<<-]<+++++++++++[>+++>+++++++++>+++++++++>+<<<<-]>-.>-.+++++++.+++++++++++.<.>>.++.+++++++..<-.>>-[[-]<]"
-
 
 -- Main
 --------------------------------------------------------------------------------
@@ -72,28 +57,20 @@ unpack3 op = (
   fromIntegral (unsafeShiftR op 12),
   fromIntegral (unsafeShiftR op 4 .&. 255))  
 
-pattern Add o a    <- (unpack3 -> (0 , o, a)) where Add o a    = pack 0  o a
-pattern Mov o      <- (unpack2 -> (1 , o)   ) where Mov o      = pack 1  o 0
-pattern Assign o a <- (unpack3 -> (2 , o, a)) where Assign o a = pack 2  o a
-pattern AddMul o a <- (unpack3 -> (3 , o, a)) where AddMul o a = pack 3  o a
-pattern SetTmp     <- (unpack1 -> 4         ) where SetTmp     = pack 4  0 0
-pattern AddTmp o   <- (unpack2 -> (5 , o)   ) where AddTmp o   = pack 5  o 0
-pattern SubTmp o   <- (unpack2 -> (6 , o)   ) where SubTmp o   = pack 6  o 0
-pattern JZ o       <- (unpack2 -> (7 , o)   ) where JZ o       = pack 7  o 0
-pattern JMP o      <- (unpack2 -> (8 , o)   ) where JMP o      = pack 8  o 0
-pattern Get o      <- (unpack2 -> (9 , o)   ) where Get o      = pack 9  o 0
-pattern Put o      <- (unpack2 -> (10, o)   ) where Put o      = pack 10 o 0
-pattern Halt       <- (unpack1 -> 11        ) where Halt       = pack 11 0 0
+pattern Add o a    <- (unpack3 -> (0 , o, a)) where Add o a    = pack 0 o a
+pattern Mov o      <- (unpack2 -> (1 , o)   ) where Mov o      = pack 1 o 0
+pattern Assign o a <- (unpack3 -> (2 , o, a)) where Assign o a = pack 2 o a
+pattern JZ o       <- (unpack2 -> (3 , o)   ) where JZ o       = pack 3 o 0
+pattern JMP o      <- (unpack2 -> (4 , o)   ) where JMP o      = pack 4 o 0
+pattern Get o      <- (unpack2 -> (5 , o)   ) where Get o      = pack 5 o 0
+pattern Put o      <- (unpack2 -> (6, o)    ) where Put o      = pack 6 o 0
+pattern Halt       <- (unpack1 -> 7         ) where Halt       = pack 7 0 0
 
 showOp :: Op -> String
 showOp = \case
   Add o a    -> "Add " ++ show o ++ " " ++ show a   
   Mov o      -> "Mov " ++ show o
   Assign o a -> "Assign " ++ show o ++ " " ++ show a
-  AddMul o a -> "AddMul " ++ show o ++ " " ++ show a
-  SetTmp     -> "SetTmp"
-  AddTmp o   -> "AddTmp " ++ show o
-  SubTmp o   -> "SubTmp " ++ show o
   JZ o       -> "JZ " ++ show o
   JMP o      -> "JMP " ++ show o
   Get o      -> "Get " ++ show o
@@ -164,40 +141,13 @@ optBlock = merge . offsets 0 where
     other                  : ops            -> other : merge ops
     []                                      -> []
 
-loopElim :: [Block] -> Maybe [Op]
-loopElim blocks = do
-  ops <- case blocks of
-    [Basic ops] -> pure ops
-    _           -> empty
-  
-  let eliminable = \case
-        Add{}    -> True
-        Mov{}    -> True
-        _        -> False
-
-  guard $ sum [o | Mov o <- ops] == 0
-  guard $ all eliminable ops
-  guard $ sum [a | Add 0 a <- ops] == (-1)
-
-  let tmpIntro = \case
-        Add 0 (-1) -> Assign 0 0
-        Add o (-1) -> SubTmp o
-        Add o 1    -> AddTmp o
-        Add o n    -> AddMul o n
-        other      -> other
-
-  pure $ SetTmp : map tmpIntro ops
-
 opt :: [Block] -> [Block]
-opt blocks = 
-  blocks >>= \case
-    Basic b  -> [Basic $ optBlock b]
-    While bs -> [While $ opt bs]
-    -- While bs -> [maybe (While bs') Basic (loopElim bs')]
-    --   where bs' = opt bs
+opt = map $ \case
+  Basic b  -> Basic $ optBlock b
+  While bs -> While $ opt bs
 
 -- Code generation
---------------------------------------------------------------------------------            
+--------------------------------------------------------------------------------
 
 linearize :: [Block] -> [Op]
 linearize blocks = evalState (go blocks) 0 [Halt] where
@@ -225,9 +175,6 @@ type Code = V.Vector Int64
 compile :: B.ByteString -> Code
 compile = V.fromList . linearize . opt . parse
 
-noOptCompile :: B.ByteString -> Code
-noOptCompile = V.fromList . linearize . parse
-
 -- Interpretation
 --------------------------------------------------------------------------------
 
@@ -244,45 +191,34 @@ run :: Code -> IO ()
 run code = do
   !(dat :: MV.IOVector Word8) <- MV.replicate memSize 0
 
-  let go :: Int -> Int -> Word8 -> IO ()
-      go !ip !i !tmp = do
+  let go :: Int -> Int -> IO ()
+      go !ip !i = do          
         case _index code ip of
           Add o a -> do
             _modify dat (+a) (i + o)
-            go (ip + 1) i tmp
+            go (ip + 1) i 
           Mov o -> do
-            go (ip + 1) (i + o) tmp
+            go (ip + 1) (i + o) 
           Assign o a -> do
             _write dat (i + o) a
-            go (ip + 1) i tmp
-          AddMul o a -> do
-            _modify dat (\x -> x + a * tmp) (i + o)
-            go (ip + 1) i tmp
-          SetTmp -> do
-            go (ip + 1) i =<< _read dat i
-          AddTmp o -> do
-            _modify dat (+tmp) (i + o)
-            go (ip + 1) i tmp
-          SubTmp o -> do
-            _modify dat (subtract tmp) (i + o)
-            go (ip + 1) i tmp
+            go (ip + 1) i 
           JZ o -> do
             _read dat i >>= \case
-              0 -> go o i tmp
-              _ -> go (ip + 1) i tmp
+              0 -> go o i 
+              _ -> go (ip + 1) i 
           JMP o -> do
-            go o i tmp
+            go o i 
           Get o -> do
             c <- getChar
             _write dat (i + o) (fromIntegral (ord c))
-            go (ip + 1) i tmp
+            go (ip + 1) i 
           Put o -> do
             b <- _read dat (i + o)
             putChar (chr (fromIntegral b))
-            go (ip + 1) i tmp
+            go (ip + 1) i 
           Halt -> do
             pure ()
           _ -> error "run: invalid opcode"
 
-  go 0 0 0 
+  go 0 0
   
