5d6e8068edbc798220f2c52602e5d3014c6b61c1
András Kovács <puttamalac@gmail.com>
2016-03-28 23:07:20 +0200

    Implement mov merging into jumps

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@
   PatternSynonyms, ViewPatterns, UnboxedTuples,
   ScopedTypeVariables #-}
 
+import Control.Arrow
 import Control.Applicative
 import Control.Monad.State.Strict
 import Data.Bits
@@ -15,11 +16,14 @@ import Text.Show
 
 import qualified Data.Attoparsec.ByteString.Char8 as P
 import qualified Data.ByteString.Char8 as B
+import qualified Data.IntMap.Strict as IM
 
 import Foreign.Marshal.Array
 import Foreign.Marshal.Alloc
 import Foreign.Storable
 import Foreign.Ptr
+
+import Debug.Trace
 
 -- Main
 --------------------------------------------------------------------------------
@@ -29,7 +33,10 @@ main = do
   !inp <- getArgs >>= \case
     path:[] -> B.readFile path
     _       -> putStrLn "usage: fastbf PATH" >> exitSuccess
-  run $ linearize $ opt $ parse inp
+  run $ linearize $ parse inp
+
+runTest = run . linearize .  parse
+printTest = mapM_ print . zip [0..] . map showOp. linearize . parse
 
 -- Opcodes
 --------------------------------------------------------------------------------
@@ -37,14 +44,14 @@ main = do
 -- 32 bit code layout, from least to most significant bits:
 --   3   bits  : opcode      (positive)
 --   8   bits  : operand arg (positive)
---   24  bits  : offset  arg (possibly negative)
+--   21  bits  : offset  arg (possibly negative)
 
 type Op = Int32
 
 pack :: Int32 -> Int -> Word8 -> Op
 pack op offset arg =
       op
-  .|. unsafeShiftL (fromIntegral arg) 3
+  .|. (unsafeShiftL (fromIntegral arg) 3)
   .|. unsafeShiftL (fromIntegral offset) 11
 
 unpack1 :: Op -> Int32
@@ -59,15 +66,29 @@ unpack3 op = (
   fromIntegral (unsafeShiftR op 11),
   fromIntegral (unsafeShiftR op 3 .&. 255))
 
+-- Need to pack the jumps differently because of sign extension
+packJ :: Int32 -> Int -> Int -> Op
+packJ op offset arg =
+    op .|. (unsafeShiftL (255 .&. fromIntegral arg) 3)
+    .|. unsafeShiftL (fromIntegral offset) 11
+
+unpackJ :: Op -> (Int32, Int, Int)
+unpackJ op = (
+  op .&. 7,
+  fromIntegral (unsafeShiftR op 11),
+  fromIntegral (unsafeShiftR (unsafeShiftL op 21) 24))      
+
 pattern Add o a    <- (unpack3 -> (0 , o, a)) where Add o a    = pack 0 o a
 pattern Assign o a <- (unpack3 -> (1 , o, a)) where Assign o a = pack 1 o a
 pattern Get o      <- (unpack2 -> (2 , o)   ) where Get o      = pack 2 o 0
-pattern Put o      <- (unpack2 -> (3, o)    ) where Put o      = pack 3 o 0
+pattern Put o      <- (unpack2 -> (3 , o)   ) where Put o      = pack 3 o 0
 
-pattern Mov o      <- (unpack2 -> (4 , o)   ) where Mov o      = pack 4 o 0
-pattern JZ o       <- (unpack2 -> (5 , o)   ) where JZ o       = pack 5 o 0
-pattern JNZ o      <- (unpack2 -> (6 , o)   ) where JNZ o      = pack 6 o 0
-pattern Halt       <- (unpack1 -> 7         ) where Halt       = pack 7 0 0
+pattern MovJZ  o a <- (unpackJ -> (4 , o, a)) where MovJZ o a  = packJ 4 o a
+pattern MovJNZ o a <- (unpackJ -> (5 , o, a)) where MovJNZ o a = packJ 5 o a
+pattern Halt       <- (unpack1 -> 6         ) where Halt       = pack 6 0 0
+
+-- | Intermediate instruction
+pattern Mov o <- (unpack2 -> (7, o::Int)) where Mov o = pack 7 o 0
 
 isCtrl :: Op -> Bool
 isCtrl op = (op .&. 7) > 3
@@ -75,10 +96,10 @@ isCtrl op = (op .&. 7) > 3
 showOp :: Op -> String
 showOp = \case
   Add o a    -> "Add " ++ show o ++ " " ++ show a
-  Mov o      -> "Mov " ++ show o
   Assign o a -> "Assign " ++ show o ++ " " ++ show a
-  JZ o       -> "JZ " ++ show o
-  JNZ o      -> "JNZ " ++ show o
+  MovJZ o a  -> "MovJZ " ++ show o ++ " " ++ show a
+  MovJNZ o a -> "MovJNZ " ++ show o ++ " " ++ show a
+  Mov o      -> "Mov " ++ show o
   Get o      -> "Get " ++ show o
   Put o      -> "Put " ++ show o
   Halt       -> "Halt"
@@ -109,6 +130,7 @@ pBasic = P.many1' $
   <|> Get 0      <$ P.char ','
   <|> Put 0      <$ P.char '.'
   <|> Assign 0 0 <$ P.string "[-]"
+  <|> Assign 0 0 <$ P.string "[+]"  
 
 pBlocks :: P.Parser [Block]
 pBlocks = P.many' $
@@ -120,20 +142,20 @@ parse = either (error "Parse error") id
       . P.parseOnly (pBlocks <* P.endOfInput)
       . B.filter (P.inClass "+-<>,.[]")
 
--- Optimization
+-- Coalescing
 --------------------------------------------------------------------------------
 
-optBlock :: [Op] -> [Op]
-optBlock = merge . offsets 0 where
+coalesceBlock :: [Op] -> ([Op], Int)
+coalesceBlock = first merge . offsets 0 where
 
   offsets o = \case
     Mov o'     : ops -> offsets (o + o') ops
-    Add _ a    : ops -> Add o a    : offsets o ops
-    Get _      : ops -> Get o      : offsets o ops
-    Put _      : ops -> Put o      : offsets o ops
-    Assign _ v : ops -> Assign o v : offsets o ops
-    other      : ops -> other      : offsets o ops
-    []               -> [Mov o | o /= 0]
+    Add _ a    : ops -> first (Add o a    :) $ offsets o ops
+    Get _      : ops -> first (Get o      :) $ offsets o ops
+    Put _      : ops -> first (Put o      :) $ offsets o ops
+    Assign _ v : ops -> first (Assign o v :) $ offsets o ops
+    other      : ops -> first (other      :) $ offsets o ops
+    []               -> ([], o)
 
   merge = \case
     Add _ 0 : ops                           -> merge ops
@@ -143,30 +165,27 @@ optBlock = merge . offsets 0 where
     other                  : ops            -> other : merge ops
     []                                      -> []
 
-opt :: [Block] -> [Block]
-opt = map $ \case
-  Basic b  -> Basic $ optBlock b
-  While bs -> While $ opt bs
-
--- Code generation
+-- Create linear code
 --------------------------------------------------------------------------------
 
 linearize :: [Block] -> [Op]
-linearize blocks = evalState (go blocks) 0 [Halt] where
+linearize = flatten where
 
-  go :: [Block] -> State Int ([Op] -> [Op])
-
-  go (Basic ops:bs) = do
-    modify (+(fromIntegral $ length ops))
-    ((ops++).) <$> go bs
-
-  go (While bs:bss) = do
-    open  <- get <* modify (+1)
-    bs'   <- go bs
-    close <- get <* modify (+1)
-    (((JZ (close + 1):) . bs' . (JNZ (open + 1):)).) <$> go bss
-
-  go [] = pure id
+  -- Unfold nested blocks into a flat list, adding jump instructions
+  flatten :: [Block] -> [Op]
+  flatten blocks = evalState (fst <$> go 0 blocks) 0 [Halt] where
+    
+    go :: Int -> [Block] -> State Int ([Op] -> [Op], Int)
+    go _ (Basic ops:bs) = do
+      let (ops', o) = coalesceBlock ops
+      modify (+(fromIntegral $ length ops'))
+      first ((ops'++).) <$> go o bs    
+    go o (While bs:bss) = do
+      open      <- get <* modify (+1)
+      (bs', o') <- go 0 bs
+      close     <- get <* modify (+1)
+      first (((MovJZ (close + 1) o:) . bs' . (MovJNZ (open + 1) o':)).) <$> go 0 bss
+    go o [] = pure (id, o)
 
 -- Interpretation
 --------------------------------------------------------------------------------
@@ -181,31 +200,22 @@ run ops = do
 
   let go :: Ptr Op -> Ptr Word8 -> IO ()
       go !ip !i = do
+        -- peekDat <- peekArray 10 dat        
         !op <- peek ip
+        -- traceShowM (showOp op, minusPtr ip code, minusPtr i dat, peekDat)
+        -- threadDelay 100000
         if isCtrl op then
           case op of
-            Mov o ->
-              case (advancePtr ip 1, advancePtr i o) of
-                (ip, i) ->
-                  peek ip >>= \case
-                    JZ o -> do
-                      peek i >>= \case
-                        0 -> go (advancePtr code o) i
-                        _ -> go (advancePtr ip 1) i
-                    JNZ o -> do
-                      peek i >>= \case
-                        0 -> go (advancePtr ip 1) i
-                        _ -> go (advancePtr code o) i
-                    Halt -> do
-                      pure ()
-            JZ o -> do
-              peek i >>= \case
-                0 -> go (advancePtr code o) i
-                _ -> go (advancePtr ip 1) i
-            JNZ o -> do
-              peek i >>= \case
-                0 -> go (advancePtr ip 1) i
-                _ -> go (advancePtr code o) i
+            MovJZ o a -> do
+              let !i' = advancePtr i a
+              peek i' >>= \case
+                0 -> go (advancePtr code o) i'
+                _ -> go (advancePtr ip 1) i'
+            MovJNZ o a -> do
+              let !i' = advancePtr i a              
+              peek i' >>= \case
+                0 -> go (advancePtr ip 1) i'
+                _ -> go (advancePtr code o) i'
             Halt -> do
               pure ()
         else do
@@ -228,3 +238,10 @@ run ops = do
   free code
   free dat
 
+
+
+hello :: B.ByteString
+hello = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+
+hello2 :: B.ByteString
+hello2 = ">++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+."
